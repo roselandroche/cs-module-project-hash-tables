e819c34b21d935c0fc983fd7b836a7a4f00e6ea6
roselandroche <r.landroche@gmail.com>
2020-08-11 22:02:39 -0400

    Slow progress

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -3,6 +3,12 @@
 
 def expensive_seq(x, y, z):
     # Your code here
+
+    # if x <= 0
+        # return y + z
+    # if x > 0
+        # expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+        
     pass
 
 
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,34 @@
+import string
+
 def word_count(s):
     # Your code here
 
+    # if the string contains NO ignored characters return an empty dictionary
+    # split string into words on whitespace
+    # all words should be lowercase
+    # IGNORE THESE CHARACTERS ->   " : ; , . - + = / \ | [ ] { } ( ) * ^ &
 
+    result = {}
+    ignored = '":;,.-+=/\|[]}{()*^&'
+
+    for char in ignored:
+        if char in s:
+            continue
+        else:
+            return result
+
+    lower_s = s.lower()
+
+    words = lower_s.split(" ")
+
+    # for item in words:
+    #     if item not in result:
+    #         result.item = 1
+    #     else:
+    #         result[item] += 1
+    
+    return result
+        
 
 if __name__ == "__main__":
     print(word_count(""))

d0452d747ecb15fb6699cd2ca227baa3abd17225
roselandroche <r.landroche@gmail.com>
2020-08-13 20:59:53 -0400

    markov is functional

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -8,8 +8,6 @@ def expensive_seq(x, y, z):
         # return y + z
     # if x > 0
         # expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
-        
-    pass
 
 
 
